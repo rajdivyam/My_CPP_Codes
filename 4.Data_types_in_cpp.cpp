3a0fa1b061b885ba7f329bc253c93df217b07c60
Divyam <rajdivyamcpr@gmail.com>
2025-09-06 21:06:52 +0530

    my fourth code

---
+++ 4.Data_types_in_cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age = 23;
+    string a = "Divyam";
+    char gender = 'M' ;
+    float weight = 70.5;
+    bool ismarried = true;
+
+    cout << age  << "\n";
+    cout << a  << "\n";                                                                          
+    cout << gender  << "\n";
+    cout << weight  << "\n";
+    cout << ismarried  << "\n";
+
+    return 0;
+}
