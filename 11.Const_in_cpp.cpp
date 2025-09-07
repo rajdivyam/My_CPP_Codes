99fce716c977718bf19a22eb9059713cc3a0f4ef
Divyam <rajdivyamcpr@gmail.com>
2025-09-07 22:48:14 +0530

    area of the circle

---
+++ 11.Const_in_cpp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+const float PI = 3.14;
+
+int main()
+{
+    int r;
+    cout << "Radius of the Circle:" ;
+    cin >> r ;
+    cout << "Area of the Circle:" ;
+    cout << PI*r*r;
+    return 0;
+}
