34229f6112bd13693466e15a56c416d5d3fc1656
Divyam <rajdivyamcpr@gmail.com>
2025-09-08 10:37:36 +0530

    Implicit type conversion

---
+++ 14.Type_conversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    char b = 'A';
+    cout << (a+b) << "\n" ;
+    float c = 2.5;
+    cout << (a+c) << "\n" ;
+    bool d = c;
+    cout << d << "\n" ;
+    return 0;
+}
