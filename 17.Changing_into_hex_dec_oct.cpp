1923b3d14be75c70fffbd26e5df6493248320214
Divyam <rajdivyamcpr@gmail.com>
2025-09-08 11:57:27 +0530

    how to change decimal numbers to octal and hexadecimal

---
+++ 17.Changing_into_hex_dec_oct.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 20;
+    int b = 24;
+    cout << hex ;
+    cout << a << " " << b << " \n" ;
+    cout << oct ;
+    cout << a << " " << b << " \n" ;
+
+
+}
