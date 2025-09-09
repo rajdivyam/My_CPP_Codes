c999134cc9276a35eb94f0505fb357b339464449
Divyam <rajdivyamcpr@gmail.com>
2025-09-09 14:45:13 +0530

    how to set width in number

---
+++ 20.Uses_of_iomanip.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout << setw(10) << 123 ;
+    // OUTPUT :- "         123"  (Right aligned with width 10)
+    return 0;
+}
