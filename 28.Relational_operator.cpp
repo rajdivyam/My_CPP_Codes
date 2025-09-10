c8e627d1ef004444cabe52c4a680fec5532095b1
Divyam <rajdivyamcpr@gmail.com>
2025-09-10 14:10:19 +0530

    uses of relational operator or also known as comparision operator

---
+++ 28.Relational_operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 20;
+    cout << (a == b) << endl;  
+    cout << (a != b) << endl;  // relational operator is used for true or false value
+    cout << (a < b) << endl;
+    cout << (a > b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a >= b) << endl;
+
+    return 0;
+}
