c0b1898f28a651217cf68e8edf68b87881ebf520
Divyam <rajdivyamcpr@gmail.com>
2025-09-10 11:39:38 +0530

    Uses of Binary Arithmetic operator

---
+++ 27.Arithmetic_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 50;
+    int b = 2;
+    cout << "a+b:" << (a+b) << endl;
+    cout << "a-b:" << (a-b) << endl;
+    cout << "a*b:" << (a*b) << endl;
+    cout << "a/b:" << (a/b) << endl;
+    cout << "a%b:" << (a%b) << endl;
+    
+    return 0;
+}
