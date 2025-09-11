dcd39e48deaf2bd60fe8b30da362da19b5cd8769
Divyam <rajdivyamcpr@gmail.com>
2025-09-11 14:20:03 +0530

    Prints using logical AND :

---
+++ 31.Logical_operator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 2;
+    bool res = ((a!=b) && cout << "Geeks") ;
+    // It prints Geeks because the before part is true
+    return 0;
+}
