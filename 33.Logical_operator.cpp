52a7d3f94e92ea022a50acd3eed04b3ec7223a2e
Divyam <rajdivyamcpr@gmail.com>
2025-09-11 16:38:10 +0530

    printing using logical OR

---
+++ 33.Logical_operator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 4;
+    bool res = ((a!=10) || cout << "Geeks") ;
+    // here the first part is false , so the other part is evaluated and printed
+    return 0;
+}
