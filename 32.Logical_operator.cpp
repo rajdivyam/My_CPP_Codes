4f6ef7786d97b5ab79e498609996750b67145643
Divyam <rajdivyamcpr@gmail.com>
2025-09-11 16:32:37 +0530

    Logical OR

---
+++ 32.Logical_operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 4;
+    bool res = ((a!=10) && cout << "Geeks") ;
+    // In logical OR operator if the first part is true then the other part is not executed.
+    return 0;
+
+}
