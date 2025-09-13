717b0e4ce7e712f9a6a01c121ab8033a9593b00d
Divyam <rajdivyamcpr@gmail.com>
2025-09-13 16:56:16 +0530

    Left shift Operator

---
+++ 40.Bitwise_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 3;
+    cout << (a << 1) << endl;
+    cout << (a << 2) << endl;
+
+    int b = 4;
+    int c = (a << b);
+    cout << c << endl;
+
+    return 0;
+}
