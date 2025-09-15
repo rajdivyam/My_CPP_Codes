34f0eb89ff2e92d7e91a31f5ac4133d8444350d7
Divyam <rajdivyamcpr@gmail.com>
2025-09-15 14:02:47 +0530

    Simple Program to calculate the No. is Odd or Even

---
+++ 50.If_else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the Number:" ;
+    cin >> a;
+    if(a%2==0)
+    {
+        cout << "The number is Even";
+    }
+    else 
+    {
+        cout << "The number is Odd" ;
+    }
+    return 0;
+}
