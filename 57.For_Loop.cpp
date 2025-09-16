919866520d0549c174c842d2f8e4027f41418673
Divyam <rajdivyamcpr@gmail.com>
2025-09-17 00:27:09 +0530

    Simple uses of for loop

---
+++ 57.For_Loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0; i<3; i++)
+    {
+        cout << "Abhimanyu Singh\n" ;
+        // This code prints Abhimanyu singh 3 times
+    }
+    return 0;
+}
