31829d68bdab77b185fa0dccbb7832d858534d69
Divyam <rajdivyamcpr@gmail.com>
2025-09-17 11:44:25 +0530

    Simple program that shows the uses of while loop

---
+++ 61.While_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i = 0;
+    cout << "Enter the Number of times you want to print my name:" ;
+    cin >> n;
+
+    while(i<n)
+    {
+        cout << "Abhimanyu Singh" << endl;
+        i++;
+    }
+
+}
