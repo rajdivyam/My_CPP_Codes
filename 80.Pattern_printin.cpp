ff225852f25b525c231c853542616dbd5b0e7f5e
Divyam <rajdivyamcpr@gmail.com>
2025-09-21 23:26:44 +0530

    Printing Inverted Triangle

---
+++ 80.Pattern_printin.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1, n;
+    cin >> n;
+
+    while (i <= n)
+    {
+        int j = 1;
+
+        // Print spaces
+        while (j <= n - i)
+        {
+            cout << ' ';
+            j++;
+        }
+
+        // Print stars
+        while (j <= n)
+        {
+            cout << '*';
+            j++;
+        }
+
+        cout << '\n';
+        i++;
+    }
+    return 0;
+}
