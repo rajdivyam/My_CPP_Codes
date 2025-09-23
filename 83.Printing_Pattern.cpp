f2e1c8cf22d2243b6b2be5400c29766974680872
Divyam <rajdivyamcpr@gmail.com>
2025-09-23 19:59:09 +0530

    Printing the number in square form for counting

---
+++ 83.Printing_Pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number:" ;
+    cin >> n;
+    int num=1;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout << num;
+            cout << " " ;
+            num++ ;
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
