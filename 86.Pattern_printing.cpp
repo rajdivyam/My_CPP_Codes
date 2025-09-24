ff8f26cf3d6307e5aa9659d84a59a06759de29a2
Divyam <rajdivyamcpr@gmail.com>
2025-09-24 14:02:50 +0530

    How to print numbers in descending order in row

---
+++ 86.Pattern_printing.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number: " ;
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=i; j>=1; j--)
+        {
+            cout << j;
+            cout << " " ;
+        }
+        cout << endl;
+    }
+    return 0;
+}
