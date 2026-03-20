63dca510a9452d00e704ff8fffd679a79bcad4cd
Divyam <rajdivyamcpr@gmail.com>
2026-03-20 22:43:21 +0530

    Greatest common Divisor

--- 100.GCD.cpp
+++ 100.GCD.cpp
@@ -6,14 +6,15 @@ int main()
     cout << "Enter the two Numbers:";
     cin >> a >> b;
 
-    int ans = 1;
+    int c = 1;
 
     for(int i=1; i<=min(a,b); i++)
     {
         if(a%i==0 && b%i==0)
         {
-            ans = i;
+            
+            c = i;
         }
     }
-    cout << ans;
+    cout << c;
 }
