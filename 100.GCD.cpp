5e579c71a0562644b28551cd5618baf441317ffc
Divyam <rajdivyamcpr@gmail.com>
2026-03-20 19:26:12 +0530

    Greatest Common Divisor

---
+++ 100.GCD.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout << "Enter the two Numbers:";
+    cin >> a >> b;
+
+    int ans = 1;
+
+    for(int i=1; i<=min(a,b); i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            ans = i;
+        }
+    }
+    cout << ans;
+}
